0584610f0622310f76798ce7efb4a3d43b21b67c
d4kk4 <jermurray2@gmail.com>
2025-11-13 19:37:55 -0600

    Basic outline for the input cli args for the server

---
+++ src/server/input.rs
@@ -0,0 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct InputParser {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    Add(Add),
+    List,
+}
+
+#[derive(Debug)]
+struct Add {
+    name : String
+}
+
+pub fn handle_input(line: String) {
+      let args = line.split_whitespace();
+      let args = std::iter::once("agent").chain(args);
+
+      match InputParser::try_parse_from(args) {
+        Ok(cli) => println!("Parsed successfully!"),
+        Err(err) => println!("Failed: {}", err),
+      }
+}
